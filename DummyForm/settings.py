ff58b17124cce06748d5ce73838e4b21ff436e6b
Ajmal Noushad <ajufhn@gmail.com>
2018-02-02 15:57:40 +0530

    Python Anywhere

--- DummyForm/settings.py
+++ DummyForm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pdstuhzv+taxl6eh$d1gtpmx8(ll&h86txjm09cn^4mk(z7p%^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ajmal.pythonanywhere.com']
 
 
 # Application definition
